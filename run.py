53844796491f5b781048f416de0dd1e18d5707ef
brandonmongo <brandonmongo@hotmail.com>
2020-07-31 11:00:10 +0000

    Creating A Message Textbox

--- run.py
+++ run.py
@@ -27,10 +27,18 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/<username>")
+@app.route("/<username>", methods=["GET", "POST"])
 def user(username):
     """ Display Chat messages"""
-    return render_template("chat.html", username=username, chat_message=messages)
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
+    return render_template("chat.html", username=username,
+                           chat_message=messages)
 
 
 @app.route("/<username>/<message>")
